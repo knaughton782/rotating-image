3f78c019e44d1992bda0df6e42f92d0d715d6832
knaughton782 <kirsten.naughton@gmail.com>
2021-03-11 11:18:02 -0700

    implement infinite spin vs button with defined degrees of rotation

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
 
-function rotateImage(degree) {
-    $('#rotate-image').animate({
-        transform: degree
-    }, {
-        step: function(now, fx) {
-            $(this).css({
-                '-webkit-transform': 'rotate(' + now + 'deg)',
-                '-moz-transform': 'rotate(' + now + 'deg)',
-                'transform': 'rotate(' + now + 'deg)'
-            });
+// function rotateImage(degree) {
+//     $('#rotate-image').animate({
+//         transform: degree
+//     }, {
+//         step: function(now, fx) {
+//             $(this).css({
+//                 '-webkit-transform': 'rotate(' + now + 'deg)',
+//                 '-moz-transform': 'rotate(' + now + 'deg)',
+//                 'transform': 'rotate(' + now + 'deg)'
+//             });
+//         }
+//     });
+// }
+
+$(function() {
+    
+    var $rota = $('.spin')
+    
+    $rota.click(function(){
+        var $this = $(this);
+        
+        if($this.data('rotating')){
+            clearInterval($this.data('rotating'));
+            $this.data('rotating', false)
+        } else {
+            $this.data('rotating', setInterval(function(){
+                var degree = $this.data('degree') || 0;
+                $this.css({ transform: 'rotate(' + degree + 'deg)'});
+                $this.data('degree', ++degree)
+            }, 5));
         }
     });
-}
+    
+});
 
-
-
-
-// $("#image").css({'transform': 'rotate(-180deg)'});
-// to
-
-// $("#image").toggleClass('flip');
-
-// $(document).ready(function() {
-//     $( ".toggle" ).click( function() {
-//         $("#image").toggleClass('flip');
-//     });
-// });
-// #image {
-//   -moz-transition: transform 1s;
-//   -webkit-transition: transform 1s;
-//   transition: transform 1s;
-// }
-
-// .flip {
-//   transform: rotate(-180deg);
-// }
-
-// $(document).ready(function() {
-//     $( ".toggle" ).click( function() {
-//       console.log($("#image").css('transform'));
-//       if ($("#image").css('transform') == 'none') {
-//         $("#image").css({'transform': 'rotate(-180deg)'});
-//       } else {
-//         $("#image").css({'transform': ''});
-//       };
-//     });
-//   });
-//   #image {
-//     -moz-transition: transform 1s;
-//     -webkit-transition: transform 1s;
-//     transition: transform 1s;
-//   }
-  
-//   .flip {
-//     transform: rotate(-180deg);
-//   }
